7b38bbc51e87b69d249a88266756dc9b229ab925
Emily Wang <qiuhuiwang0923521@126.com>
2016-05-12 21:54:55 +0800

    HW1 completed.

--- HW1/checkEmail.js
+++
@@ -1,8 +0,0 @@
-function checkEmail(){
-var email=document.getElementById("email").value;
-
-function isMail(str){
-var reg=/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
-return reg.test(str);
-}
-}
--- HW1/myFunction.js
+++ HW1/myFunction.js
@@ -1,9 +1,18 @@
-function doSubmit() {
+function checkEmail() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    if (email != "" && password != "") {
-        console.log(email);
-        console.log(password);
-    } else
-        document.getElementById("p1").innerHTML = "Please input your username and password";
+    if (!isEmail(email)) {
+        alert("This is not an email, please input a valid email address");
+        document.getElementById("email").focus();
+        return false;
+    } else {
+        //console.log(email);
+        //console.log(password);
+        document.getElementById("b1").value = "success";
+    }
+}
+
+function isEmail(str) {
+    var reg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
+    return reg.test(str);
 }
