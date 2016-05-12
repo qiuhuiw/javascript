7049c92a7d115508dc66ee510386af03ed8f72e8
Emily Wang <qiuhuiwang0923521@126.com>
2016-05-12 22:48:19 +0800

    HW1 V4

--- HW1/myFunction.js
+++ HW1/myFunction.js
@@ -1,8 +1,12 @@
 function checkEmail() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+
+    //如果email输入不符合格式，则弹出message，点击OK 清空输入的同时光标定位到email输入框里。否则将submit button value 改为success
     if (!isEmail(email)) {
         alert("This is not an email, please input a valid email address");
+        document.getElementById("email").value="";
+        document.getElementById("password").value="";
         document.getElementById("email").focus();
         return false;
     } else {
@@ -11,6 +15,8 @@ function checkEmail() {
         document.getElementById("b1").value = "success";
     }
 }
+
+//email正则表达式并返回其验证结果
 
 function isEmail(str) {
     var reg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
